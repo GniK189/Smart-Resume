167034b3d94e5952011928b2876f816e1424cc0e
NPTrungHieu <nptrunghieu1809@gmail.com>
2026-02-25 22:56:11 +0700

    backend: setup mongodb

---
+++ backend/configs/db.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Database connection successfully");
+    });
+
+    let mongodbURI = process.env.MONGODB_URI;
+    const projectName = "smart-resume";
+
+    if (!mongodbURI) {
+      throw new Error("MONGODB_URI variable not set");
+    }
+
+    if (mongodbURI.endsWith("/")) {
+      mongodbURI = mongodbURI.slice(0, -1);
+    }
+
+    await mongoose.connect(`${mongodbURI}/${projectName}`);
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error);
+  }
+};
+
+export default connectDB;
