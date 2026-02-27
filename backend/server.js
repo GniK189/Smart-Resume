35dcf8b395df19394afecfb18fad9d2175c7646f
NPTrungHieu <nptrunghieu1809@gmail.com>
2026-02-27 15:12:29 +0700

    deploy setup

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ import userRouter from "./routes/user.routes.js";
 import resumeRouter from "./routes/resume.routes.js";
 import aiRouter from "./routes/ai.routes.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -14,13 +20,23 @@ await connectDB();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Server is live...");
-});
-
 app.use("/api/users", userRouter);
 app.use("/api/resumes", resumeRouter);
 app.use("/api/ai", aiRouter);
+
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+
+  app.use(express.static(frontendPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running in Development mode...");
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
